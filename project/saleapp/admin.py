0d7933c5e456cea838d18b38f18b9ab88fbff11e
kaneki-sama <ngovanhau1999@gmail.com>
2020-11-17 20:24:43 +0700

    sua cho dep

--- project/saleapp/admin.py
+++ project/saleapp/admin.py
@@ -6,11 +6,19 @@ from flask_login import current_user, logout_user
 
 from saleapp.models import maybay, khchuyenbay, chuyenbay, khachhang, sanbay, sanbaytrunggian, loaighe
 
+class AuthenticatedView(BaseView):
+    column_display_pk = True
+    can_create = True
+    can_edit = True
+    can_export = True
+    can_delete = True
+    def is_accessible(self):
+        return current_user.is_authenticated
+
 class ContactView(BaseView):
     @expose('/')
     def index(self):
         return self.render('admin/contact.html')
-    #
     def is_accessible(self):
         return current_user.is_authenticated
 
@@ -23,12 +31,11 @@ class LogoutView(BaseView):
         return current_user.is_authenticated
 
 
-admin.add_view(ContactView(name='Liên hệ'))
 admin.add_view(LogoutView(name='Logout'))
-admin.add_view(ModelView(maybay, db.session))
-admin.add_view(ModelView(khchuyenbay, db.session))
-admin.add_view(ModelView(chuyenbay, db.session))
-admin.add_view(ModelView(khachhang, db.session))
-admin.add_view(ModelView(sanbay, db.session))
-admin.add_view(ModelView(sanbaytrunggian, db.session))
-admin.add_view(ModelView(loaighe, db.session))
+admin.add_view(ModelView(maybay, db.session, name='Máy bay'))
+admin.add_view(ModelView(khchuyenbay, db.session, name='Vé'))
+admin.add_view(ModelView(chuyenbay, db.session, name='Chuyến bay'))
+admin.add_view(ModelView(khachhang, db.session, name='Khách hàng'))
+admin.add_view(ModelView(sanbay, db.session, name='Sân bay'))
+admin.add_view(ModelView(sanbaytrunggian, db.session, name='Sân bay TG'))
+admin.add_view(ModelView(loaighe, db.session, name='Loại Vé'))
